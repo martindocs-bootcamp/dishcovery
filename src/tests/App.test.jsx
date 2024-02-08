3a461e97d6a067df293be3d2d695b85a96a62bed
Marcin Tatarski <martin.tatarski>
2024-02-08 19:56:32 +0000

    Remove App.test.jsx

--- src/tests/App.test.jsx
+++
@@ -1,7 +0,0 @@
-import { it } from 'vitest'
-import { render } from '@testing-library/react';
-import App from '../App';
-
-it('<App /> component renders without error', () => {
-    render(<App />);
-});

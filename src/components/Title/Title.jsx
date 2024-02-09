f91e1f161d963aee13d0b702bdc88f0cde453ef3
rrana5106 <rrana5106@hotmail.com>
2024-02-09 17:45:15 +0000

    updating the scss file of tile and maincontent

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -4,6 +4,6 @@ function Title(props) {
   // Destructuring the data object from props and extracting the meals property, assigning it to the variable name meals
   const { meals } = props.data;
 
-  return <h2>{meals[0].strMeal}</h2>;
+  return <h2 className="title">{meals[0].strMeal}</h2>;
 }
 export default Title;

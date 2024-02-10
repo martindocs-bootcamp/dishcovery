56478120bafe7b3bbc5beffe3053d16f37de4457
Marcin Tatarski <martin.tatarski>
2024-02-10 16:47:27 +0000

    Update Recipe page to display data

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -1,11 +1,3 @@
-
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import { MainContent, Title, Ingredients, ImageFood } from "../../components";
-import { useGlobalContext } from "../../hooks/useGlobalContext";
-import { Link } from "react-router-dom";
-
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
@@ -19,50 +11,10 @@ import { useGlobalContext } from '../../hooks/useGlobalContext';
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
 
-
 import data from "../../data/data.json";
 import edamanData from "../../data/edamam.json";
 
 const Recipe = () => {
-
-  const { edamamAPI } = useGlobalContext();
-
-  if (edamamAPI.length === 0) {
-    return <section className="">Search for recipe</section>;
-  }
-
-  return (
-    edamamAPI.length !== 0 && (
-      <main className="home">
-        <Row>
-          <Col>
-            {/* <Title data={data}/>  */}
-            <Title edamanData={edamanData}/>
-            <div className="home-btn">
-              <Link to="/recipe">
-                <Button vatiant="primary">Nutritions</Button>
-              </Link>
-              <Link to="/drinks">
-                <Button vatiant="primary">Drinks</Button>
-              </Link>
-              <Button variant="primary">Likes</Button>
-            </div>
-            <section className="home-content">
-              <MainContent edamanData={edamanData} />
-            </section>
-          </Col>
-          <Col>
-            <ImageFood data={data} />
-            {/* <Ingredients meals={data.meals[0]}/> */}
-            <Ingredients edamanData={edamanData} />
-          </Col>
-        </Row>
-      </main>
-    )
-  );
-};
-
-
   const { edamamAPI, fetchEdamamRecipes } = useGlobalContext();  
 
   useEffect(() => {
@@ -82,7 +34,7 @@ const Recipe = () => {
     <main className="home">
       <Row>
         <Col>
-        <Title data={data}/>            
+        <Title edamanData={edamanData}/>            
           <div className='home-btn'>           
             <Link to="/recipe">
               <Button vatiant="primary">Nutritions</Button>
@@ -93,17 +45,17 @@ const Recipe = () => {
             <Button variant='primary'>Likes</Button>
           </div>
           <section className='home-content'>
-            <MainContent data = {data}/>
+            <MainContent edamanData={edamanData}/>
           </section>
         </Col>
         <Col>
-          <ImageFood data = {data} />
-          <Ingredients meals={data.meals[0]}/>
+          <ImageFood data={data} />
+          <Ingredients edamanData={edamanData}/>
         </Col>
       </Row>
     </main>
   )
 }
   
-
 export default Recipe;
+  

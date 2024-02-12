6685b1c48b169ea8f8dce4e81d1330acb9aff966
rrana5106 <rrana5106@hotmail.com>
2024-02-12 18:32:45 +0000

    update the code

--- src/components/ImageFood/ImageFood.jsx
+++ src/components/ImageFood/ImageFood.jsx
@@ -1,10 +1,8 @@
-function ImageFood({image}) {
-  
-  return (    
-    // Render the image with its URL as 'src' and set the width 
-    <img className="imageFood" src={image} alt="Food" style={{width:"250px"}}/>
-       
-  )
+function ImageFood({ image }) {
+  return (
+    // Render the image with its URL as 'src' and set the width
+    <img className="imageFood" src={image} alt="Food" />
+  );
 }
 
 export default ImageFood;

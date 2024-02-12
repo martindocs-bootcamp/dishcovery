17881cacd15eae6929d3674bf89e54a231a2042f
Marcin Tatarski <martin.tatarski>
2024-02-12 15:47:52 +0000

    Add navigation menu

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,21 @@
 import { Link } from 'react-router-dom';
 import Logo from '../Logo/Logo';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Navigation = () => {
-  return (
-    <nav className='nav'>      
-      <Link to="/">
-        <Logo />
-      </Link>
-    </nav>
+  return (    
+    <Navbar className='nav'>
+      <Container className='nav-container'>
+        <Link to="/"><Logo /></Link>        
+        <Nav className="me-auto nav-menu">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Favourites</Nav.Link>
+          <Nav.Link href="#pricing">Contact</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 };
 

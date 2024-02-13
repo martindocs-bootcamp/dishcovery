29070dd2a51909ced004f211d1f9e61746cee337
Martin Tatarski <martin.tatarski@gmail.com>
2024-02-13 11:00:45 +0000

    Second test

--- functions/accessVar.js
+++
@@ -1,8 +0,0 @@
-export const handler = async function (event, context) {
-    const value = import.meta.env.VITE_EDAMAM_APP_ID;
-  
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Value of VITE_EDAMAM_APP_ID is ${value}.` }),
-    };  
-  };

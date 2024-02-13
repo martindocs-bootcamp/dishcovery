e5ea590046c264ea00d0101864862ebab98eae1f
TuPu <141525093+TuuPuu@users.noreply.github.com>
2024-02-13 02:11:16 +0000

    First iteration of save button that creates a favourites modal pop up- Don't merge yet as was created with test data

--- src/components/Favourites/Favourites.jsx
+++ src/components/Favourites/Favourites.jsx
@@ -1,11 +1,37 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
+const MyModal = ({ show, handleClose }) => {
+  // This part is to etrieve the saved favorite meal from local storage
+  const savedMeal = JSON.parse(localStorage.getItem("favoriteMeal"));
 
-const Favourites = () => {
   return (
-    <section className="fav">
-        Favourites
-    </section>
-  )
-}
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Favourites ♥️</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* This part displays the saved favorite meal */}
+          {savedMeal ? (
+            <p>{savedMeal.strMeal}</p>
+          ) : (
+            <p>No favorite meal saved</p>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            className="close-btn"
+            variant="secondary"
+            onClick={handleClose}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
 
-export default Favourites;
+export default MyModal;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,38 +1,53 @@
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { MainContent, Likes } from '../../components'; 
-
+import React, { useState } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { MainContent, Likes } from "../../components";
 import data from "../../data/data.json";
-import Title from '../../components/Title/Title';
-import Ingredients from '../../components/Ingredients/Ingredients';
-import ImageFood from '../../components/ImageFood/ImageFood';
-
-
-
+import Title from "../../components/Title/Title";
+import Ingredients from "../../components/Ingredients/Ingredients";
+import ImageFood from "../../components/ImageFood/ImageFood";
+import MyModal from "../../components/Favourites/Favourites";
 
 const Home = () => {
-    return (
-      <main className="home">
-        <Row>
-          <Col>
-          <Title data={data}/>
-            
-            <div className='home-btn'>
-              links to steps or nutritions or drinks
-              <Likes />
-            </div>
-            <section className='home-content'>
-              <MainContent data = {data}/>
-            </section>
-          </Col>
-          <Col>
-            <ImageFood data = {data} />
-            <Ingredients meals={data.meals[0]}/>
-          </Col>
-        </Row>
-      </main>
-    )
-  }
-  
-  export default Home;
-  
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const handleButtonClick = () => {
+    const selectedMeal = data.meals[0];
+    // Store the selected meal in local storage
+    localStorage.setItem("favoriteMeal", JSON.stringify(selectedMeal));
+    // Show the modal
+    setShowModal(true);
+  };
+
+  return (
+    <main className="home">
+      <Row>
+        <Col>
+          <Title data={data} />
+          <button className="fav-btn" onClick={handleButtonClick}>
+            {" "}
+            &hearts;{" "}
+          </button>{" "}
+          <div className="home-btn">
+            links to steps or nutritions or drinks
+            <Likes />
+          </div>
+          <section className="home-content">
+            <MainContent data={data} />
+          </section>
+        </Col>
+        <Col>
+          <ImageFood data={data} />
+          <Ingredients meals={data.meals[0]} />
+        </Col>
+      </Row>
+      {showModal && <MyModal show={showModal} handleClose={handleModalClose} />}
+    </main>
+  );
+};
+
+export default Home;

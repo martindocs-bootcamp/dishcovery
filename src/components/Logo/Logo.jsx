5297c4cef0dbcf4a25db6c26672cd154e00744e0
Marcin Tatarski <martin.tatarski>
2024-02-15 17:26:42 +0000

    Deploy to Netlify

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,4 +1,4 @@
-import logo from "../../assets/logo2.png";
+import logo from "../../assets/logo.png";
 
 const Logo = () => {
   return <img src={logo} className="logo" alt="Dishcovery logo" />;
--- src/context/initState.js
+++ src/context/initState.js
@@ -4,7 +4,5 @@ export const initialState = {
     edamamAPI: [],  // Array to store Edamam API recipes
     recipeID: '', // ID of a specific recipe
     drinksAPI: [], // Array to store drink recipes   
-    search: '', // Search term for recipes
-    sendingEmail: false, // Flag to track whether an email is being sent
-    errorMessage: null,  // Error message to display in case of API or email errors       
+    search: '', // Search term for recipes     
 }
